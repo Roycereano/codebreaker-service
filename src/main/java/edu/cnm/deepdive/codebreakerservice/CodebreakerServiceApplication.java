42bb45384727300c4a18aef14eb8822c9ac63882
Royce Reano <reano.royce@gmail.com>
2021-10-27 10:06:02 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/codebreakerservice/CodebreakerServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.codebreakerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebreakerServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CodebreakerServiceApplication.class, args);
+  }
+
+}
